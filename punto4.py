637d52e708e20fd7e6abce08f8bf90622e69b9a5
ORTIZ CANAS NELSON <nortiz9@cuc.edu.co>
2022-03-02 21:00:37 -0500

    commit punto 4

---
+++ punto4.py
@@ -0,0 +1,16 @@
+alumnonotaT1 = float (input("taller 1: "))
+alumnonotaT2 = float (input("taller 2: "))
+alumnonotaT3 = float (input("taller 3: "))
+
+
+examen = float (input("examen nota: "))
+
+proyectofinal = (input("proyecto: "))
+
+
+notafinal = alumnonotaT1 + alumnonotaT2 + alumnonotaT3 /3 *0.50
+examenalumno = examen * 0.30
+proyecto = proyectofinal*0.20
+
+notaalumno = notafinal + examenalumno + proyecto
+print("La nota final del alumno es: ", notaalumno)
