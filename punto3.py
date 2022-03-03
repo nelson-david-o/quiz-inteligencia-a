5bb597be3ce3ec3c066f8755b155ee4ff0c52b2f
ORTIZ CANAS NELSON <nortiz9@cuc.edu.co>
2022-03-02 20:40:08 -0500

    commit punto 3

---
+++ punto3.py
@@ -0,0 +1,8 @@
+usuario_vendedor = float (input("sueldo base del vendedor "))
+usuario_vendedor = float (input("ventas del vendedor por mes "))
+
+porcentaje = 0.15
+comision_vendedor = usuario_vendedor * (porcentaje/100)
+
+print ("Valor ganado por comision y valor total del vendedor",  comision_vendedor)
+
