b520921134c172041cad0803798d5d17a815747a
ORTIZ CANAS NELSON <nortiz9@cuc.edu.co>
2022-03-02 20:17:10 -0500

    commit punto 2

---
+++ punto2.py
@@ -0,0 +1,9 @@
+user1 = float(input ("digite donacion del user 1: "))
+user2 = float(input ("digite donacion del user 2: "))
+user3 = float(input ("digite donacion del user 3: "))
+
+empresaLocal = user1 + user2 + user3
+
+print (("El porcentaje  del usuario 1 es: ", (user1/empresaLocal)*100 , 
+" el porcentaje de la persona 2 es: ",  (user2/empresaLocal)*100 ,
+" El porcentaje de la persona 3 es: ", (user3/empresaLocal)*100 ))
